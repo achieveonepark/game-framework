7d150024ce0d6672f501109f74eef857d92df65a
achieveonepark <park_achieveone@naver.com>
2024-11-20 23:44:46 +0900

    feat: LISCENSE 추가, repository 추가

---
+++ Runtime/Extensions/Array/BitString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GameFramework
+{
+	struct BitString
+	{
+		public byte[] stream;
+
+		public long Length
+		{
+			get { return stream.LongLength * 8; }
+		}
+
+		public BitString(byte[] stream)
+		{
+			this.stream = stream;
+		}
+
+		public bool this[ulong index]
+		{
+			get
+			{
+				//This - 'appears to work'...
+				return
+					(stream[(ulong)Math.Floor((double)index / 8.0)]
+					 & (1 << (int)(index % 8))
+					) > 0;
+			}
+			//...and so does this
+			set
+			{
+				if (value)
+				{
+					stream[(ulong)Math.Floor((double)index / 8.0)]
+						|=
+						(byte)(1 << (int)(index % 8)); //Turn the bit on
+
+				}
+				else
+				{
+					//{TODO} Make this turn the bit off.... I think
+					stream[(ulong)Math.Floor((double)index / 8.0)]
+						|=
+						(byte)(1 << (int)(index % 8)); //Turn the bit on
+
+					stream[(ulong)Math.Floor((double)index / 8.0)]
+						^=
+						(byte)(1 << (int)(index % 8)); //Toggle the bit (Toggle from on -> off)
+				}
+			}
+		}
+
+	}
+}
